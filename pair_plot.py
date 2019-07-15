e4bb3ac9b42db3859b41f7c12de755c4a417943a
eparisot <eparisot@student.42.fr>
2019-07-16 00:08:30 +0200

    plots OK

--- pair_plot.py
+++ pair_plot.py
@@ -6,7 +6,47 @@ import click
 @click.argument("data_file", type=click.Path(exists=True))
 @click.option("-sep", "sep", default=",", help="csv separator")
 def main(data_file, sep):
-    pass
+    classes_column = "Hogwarts House"
+    data = read_data(data_file, sep)
+    num_data = get_numerics(data, False)
+    class_list = get_classes(data, classes_column)
+    fig = plt.figure()
+    # for each matter
+    for j, key_a in enumerate(num_data.keys()):
+        for i, key_b in enumerate(num_data.keys()):
+            ax = fig.add_subplot(len(num_data), len(num_data), len(num_data) * j + i + 1)
+            if j == len(num_data)-1:
+                ax.set(xlabel=key_b)
+            if i == 0:
+                ax.set(ylabel=key_a)
+            if key_b != key_a:
+                classes = []
+                # for each class
+                for c in class_list:
+                    c_tab_a = []
+                    c_tab_b = []
+                    # for each note 
+                    for idx, val in enumerate(data):
+                        if val[classes_column] == c:
+                            c_tab_a.append(float(num_data[key_a][idx]))
+                            c_tab_b.append(float(num_data[key_b][idx]))
+                    classes.append((c_tab_a, c_tab_b))
+                for cat in classes:
+                    ax.scatter(cat[0], cat[1], alpha=0.5)
+            else:
+                # for each class
+                classes = []
+                for c in class_list:
+                    c_tab = []
+                    # for each note
+                    for idx, val in enumerate(num_data[key_a]):
+                        if idx in class_list[c]:
+                            c_tab.append(float(val))
+                    classes.append(c_tab)
+                for cat in classes:
+                    ax.hist(cat, alpha=0.5)
+    fig.tight_layout()
+    plt.show(block = True)
 
 if __name__ == "__main__":
     main()
--- scatter_plot.py
+++ scatter_plot.py
@@ -16,8 +16,10 @@ def main(data_file, sep):
         for i, key_b in enumerate(num_data.keys()):
             if key_b != key_a:
                 ax = fig.add_subplot(len(num_data), len(num_data), len(num_data) * j + i + 1)
-                #ax.set_title(key_a + "/" + key_b)
-                ax.axis("off")
+                if j == len(num_data)-1:
+                    ax.set(xlabel=key_b)
+                if i == 0:
+                    ax.set(ylabel=key_a)
                 classes = []
                 # for each class
                 for c in class_list:
